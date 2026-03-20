9163a52ada47362c4ee2b87ec3b83819eaffab77
PratikNaskar11 <pratiknaskar11@gmail.com>
2026-03-20 14:14:27 +0530

    Fixed Login

--- backend/src/main/java/com/bplus/backend/repo/VolunteerApplicationRepo.java
+++ backend/src/main/java/com/bplus/backend/repo/VolunteerApplicationRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VolunteerApplicationRepo extends JpaRepository {
+public interface VolunteerApplicationRepo extends JpaRepository<VolunteerApplication, Long> {
     boolean existsByEmail(String email);
 }
